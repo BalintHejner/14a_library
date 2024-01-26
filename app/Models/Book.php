8339af0d0718b138c26bb970c030681aea16567d
BalintHejner <balinthejner204@gmail.com>
2024-01-26 12:30:35 +0100

    First commit

---
+++ app/Models/Book.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    //protected $fillable = ['title', 'ISBN', 'pages', 'description', 'hard_cover', 'category_id'];
+    protected $guarded = ['id'];
+
+    /**
+     * Get the category that owns the Book
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
